84a016bb9e82ad038ab7851fcb193c585ad7c4fa
Dezmond152 <rossomahavit@gmail.com>
2025-01-12 15:52:56 +0200

    background sfx tests

--- sfx.js
+++ sfx.js
@@ -1,14 +1,18 @@
 window.SFX = {
   doorEnter : new Audio('./sfx/DoorEnter.ogg'),
   doorExit : new Audio('./sfx/DoorExit.ogg'),
+  House : new Audio ('./sfx/House.ogg'),
+  House1 : new Audio ('./sfx/House1.ogg'),
 }
 
-const setVolumeForAllSFX = (volume) => {
-  for (let sound in window.SFX) {
-    if (window.SFX.hasOwnProperty(sound)) {
-      window.SFX[sound].volume = volume;
-    }
-  }
-};
 
-setVolumeForAllSFX(0.25);
+
+window.SFX.doorEnter.volume = 0.25;
+window.SFX.doorExit.volume = 0.25;
+window.SFX.House.volume = 0.05;
+window.SFX.House1.volume = 0.05;
+
+
+document.addEventListener('click', () => {
+  window.SFX.House1.play();
+});
